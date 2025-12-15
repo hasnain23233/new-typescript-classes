bd97b4762bbba0d7392458a5159758f54f7d7909
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-12-15 19:56:17 +0500

    starting the typescript classes

---
+++ class1/app.js
@@ -0,0 +1,10 @@
+///number data types 
+var num1 = 10;
+console.log(num1);
+var data = 39;
+console.log(data);
+//types of number
+var types1 = 4099;
+var hexa = 0x19248;
+var binary = 5;
+//these are the importent concept of the typscript
---
+++ class1/app.ts
@@ -0,0 +1,14 @@
+///number data types 
+var num1: number = 10
+console.log(num1)
+
+var data3 : number | string = 39
+console.log(data3)
+
+//types of number
+
+var types1 = 0o10003;
+var hexa = 0x19248;
+var binary = 0b0101;
+
+//these are the importent concept of the typscript
