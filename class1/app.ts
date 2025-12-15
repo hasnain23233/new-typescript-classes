d69d30663615eb0446f7300c6a6dab680ad0ed49
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-12-15 20:29:33 +0500

    study about string and boolean

--- class1/app.ts
+++ class1/app.ts
@@ -12,3 +12,27 @@ var hexa = 0x19248;
 var binary = 0b0101;
 
 //these are the importent concept of the typscript
+
+
+//string value
+
+var prson1 : string = "wiki"
+var personNo: number = 3
+
+var fullData : string = `my name is ${prson1} and the number is ${personNo}`
+
+
+var number : number = 93
+var convetString : string = number.toString()
+console.log(convetString)
+
+
+///boolean
+
+var blee: boolean = true
+console.log(blee)
+
+var convertingBolIntoString : string = blee.toString()
+console.log(convertingBolIntoString)
+
+
