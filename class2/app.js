bedc68308b8877c150eda7812b2b39ea0b0bc688
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-12-16 16:43:28 +0500

    working on a small project using the typescript with the html

---
+++ class2/app.js
@@ -0,0 +1,7 @@
+function getInfo() {
+    var firstInput = document.getElementById('name');
+    var secondInput = document.getElementById('message');
+    var name = firstInput.value;
+    var message = secondInput.value;
+    console.log(name, '  ', message);
+}
---
+++ class2/app.ts
@@ -0,0 +1,9 @@
+function getInfo(){
+    const firstInput = document.getElementById('name') as HTMLInputElement
+    const secondInput = document.getElementById('message') as HTMLInputElement
+
+    const name : string = firstInput.value
+    const message: string = secondInput.value
+
+    console.log(name , '  ' , message)
+}
