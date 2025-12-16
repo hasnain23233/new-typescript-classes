08b14d0ae1f0a247f74d7f14025071aff37f5cbc
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-12-16 17:04:11 +0500

    now we working on Array and how to use them in typescript

---
+++ class3/app.js
@@ -0,0 +1,10 @@
+//Array in Typescript and how to use them 
+var arr = [2, 4, 45];
+console.log(arr);
+arr.push(3);
+var arrStr = ['Yellow', 'red', 'white'];
+console.log(arrStr);
+var arrNumber = [12, 32, 53];
+console.log(arrNumber);
+var arrString = ['Tofail', 'Inayat', 'Ehtisham'];
+console.log(arrString);
---
+++ class3/app.ts
@@ -0,0 +1,13 @@
+//Array in Typescript and how to use them 
+
+let arr: Array<number> = [2 , 4 , 45]
+console.log(arr)
+arr.push(3)
+let arrStr: Array<string> = ['Yellow' , 'red' , 'white']
+console.log(arrStr)
+
+let arrNumber: number[] = [12 , 32 , 53]
+console.log(arrNumber)
+
+let arrString: string[] = ['Tofail' , 'Inayat' , 'Ehtisham']
+console.log(arrString)
