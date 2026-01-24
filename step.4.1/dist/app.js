f83cdb3e9e01520c4ca77d4c753047ff2eeabdd8
Hasnain Khushi <hasnainkhushii@gmail.com>
2026-01-24 17:22:35 +0500

    working on the object and object aliased

---
+++ step.4.1/dist/app.js
@@ -0,0 +1,22 @@
+console.log("In this class we learn about object aliased");
+//anonymus
+let teacher = {
+    name: "Zahid",
+    profile: "Linkedin"
+};
+console.log(teacher.name);
+console.log(teacher.profile);
+let userData = {
+    name: "hasnain khushi",
+    exp: 9,
+    email: "hasnainkhushii@gmail.com"
+};
+console.log(userData.email);
+console.log(userData.name);
+console.log(userData.exp);
+let ManagerData = {
+    name: "Aslam",
+};
+console.log(ManagerData.name);
+export {};
+//# sourceMappingURL=app.js.map
---
+++ step.4.1/src/app.ts
@@ -0,0 +1,35 @@
+console.log("In this class we learn about object aliased")
+
+//anonymus
+let teacher: {name: string , profile: string} = {
+    name: "Zahid",
+    profile: "Linkedin"
+}
+
+console.log(teacher.name)
+console.log(teacher.profile)
+
+//Aliased object Types
+type dataTypes = {
+    name: string , exp: number , email: string
+}
+
+let userData : dataTypes = {
+    name: "hasnain khushi",
+    exp: 9,
+    email: "hasnainkhushii@gmail.com"
+}
+console.log(userData.email)
+console.log(userData.name)
+console.log(userData.exp)
+
+//interface
+interface Manager {
+    name: string , 
+    subdirector ?:number // ? using for objection
+}
+
+let ManagerData: Manager = {
+    name : "Aslam",
+}
+console.log(ManagerData.name)
