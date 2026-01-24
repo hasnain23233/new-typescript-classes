6a5483cce1ed4937553d433d830824ea6baee93b
Hasnain Khushi <hasnainkhushii@gmail.com>
2026-01-24 17:07:58 +0500

    working on objects and how to use them in a project

---
+++ step.4/dist/app.js
@@ -0,0 +1,12 @@
+console.log('In this class we learn about the objects in typescript and how to use them');
+const personData = {
+    name: "Hasnain Khushi",
+    email: "hasnainkhushii@gmail.com",
+    phone: 3472934
+};
+let user = personData.name;
+let userPhone = personData.phone;
+console.log(user, userPhone);
+console.log(personData.email);
+export {};
+//# sourceMappingURL=app.js.map
---
+++ step.4/src/app.ts
@@ -0,0 +1,12 @@
+console.log('In this class we learn about the objects in typescript and how to use them')
+
+const personData = {
+    name: "Hasnain Khushi",
+    email: "hasnainkhushii@gmail.com",
+    phone: 3472934
+}
+let user: string = personData.name
+let userPhone: number = personData.phone
+console.log(user , userPhone)
+
+console.log(personData.email)
