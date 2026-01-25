3655974f40a770c1ec008e6276b0a8dfeac238b6
Hasnain Khushi <hasnainkhushii@gmail.com>
2026-01-25 23:52:50 +0500

    in this class we learn about interface using on the object

---
+++ step.4.2/dist/app.js
@@ -0,0 +1,16 @@
+console.log("This is class we learn about object literals");
+let user1 = {
+    name: "Fahad"
+};
+let user2 = {
+    phone: 473957
+};
+user1 = user1;
+user2 = user2;
+console.log(user1);
+console.log(user2);
+let tofailEmp = { phone: 8394, name: "tofail", exp: 5, conform: true, country: 'pakistan', city: "Gilgit" };
+tofailEmp = { phone: 8394, name: "tofail", exp: 5, conform: true, country: 'pakistan', city: "Gilgit" };
+console.log(tofailEmp);
+export {};
+//# sourceMappingURL=app.js.map
---
+++ step.4.2/src/app.ts
@@ -0,0 +1,35 @@
+console.log("This is class we learn about object literals")
+interface defer {
+    name: string
+}
+
+interface checker {
+    phone : number
+}
+
+
+
+let user1 : defer = {
+    name: "Fahad"
+}
+let user2 : checker = {
+    phone : 473957
+}
+
+user1 = user1
+user2 = user2
+
+console.log(user1)
+console.log(user2)
+
+interface personDetails {
+    name : string , phone: number , exp : number , conform : boolean
+}
+interface moreDetails extends personDetails {
+    country : string , city: string
+}
+
+let tofailEmp : moreDetails = {phone : 8394 , name: "tofail" ,   exp : 5 , conform : true , country: 'pakistan' , city: "Gilgit"}
+tofailEmp = {phone : 8394 , name: "tofail" ,   exp : 5 , conform : true,  country: 'pakistan' , city: "Gilgit"}
+
+console.log(tofailEmp)
