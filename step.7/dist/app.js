9f10f8400d1d5dc8d94241dec6114f1b25257b25
Hasnain Khushi <hasnainkhushii@gmail.com>
2026-01-29 20:10:03 +0500

    In this class we learn about array and its property

---
+++ step.7/dist/app.js
@@ -0,0 +1,13 @@
+console.log("In This class we learn about the typescript and how to use it in programming");
+let numberStr = [1, 2, 4, 5];
+console.log(numberStr);
+let numberStr2 = [1, 5, 6, 6];
+console.log(numberStr2);
+let textStore = ['data', 'programming', 'javascript'];
+console.log(textStore);
+let textStore2 = ["JavaScript", "TypeScript", "python"];
+console.log(textStore2);
+let mixStr = ["Hasnain", 39, true, 9304];
+console.log(mixStr);
+export {};
+//# sourceMappingURL=app.js.map
---
+++ step.7/src/app.ts
@@ -0,0 +1,19 @@
+console.log("In This class we learn about the typescript and how to use it in programming")
+
+let numberStr: number[] = [1 , 2 , 4 ,5 ]
+console.log(numberStr)
+
+let numberStr2: Array<number> = [1 , 5 ,6 ,6]
+console.log(numberStr2)
+
+let textStore : string[] = ['data' , 'programming' , 'javascript']
+console.log(textStore)
+
+let textStore2: Array<string> = ["JavaScript" , "TypeScript" , "python"]
+console.log(textStore2)
+
+
+type store =  [string , number , boolean , number]
+
+let mixStr : store = ["Hasnain" , 39 , true , 9304]
+console.log(mixStr)
