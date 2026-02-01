164b629dcb01418bfa642f9b4da1141af2941f7b
Hasnain Khushi <hasnainkhushii@gmail.com>
2026-02-01 22:50:34 +0500

    In this class we learn about turpel

---
+++ step.10/dist/app.js
@@ -0,0 +1,15 @@
+let myTurbel;
+myTurbel = ["Ehtisham", 83, true];
+console.log(myTurbel);
+let store = ["Apple", "Banana", 923, false];
+console.log(store);
+store.push("3459345");
+console.log(store);
+const staf = [
+    ["Mashruf", "The was a medical bilar", true],
+    ["Aslam", "The was a developer", true],
+    ["Khan", "The was a engineer", true]
+];
+console.log(staf);
+export {};
+//# sourceMappingURL=app.js.map
---
+++ step.10/src/app.ts
@@ -0,0 +1,23 @@
+let myTurbel : [string , number , boolean]
+
+myTurbel = ["Ehtisham" , 83 , true]
+console.log(myTurbel)
+
+type DataFetching = [string , string , number , boolean]
+
+let store: DataFetching = ["Apple" , "Banana" , 923 , false]
+console.log(store)
+store.push("3459345")
+console.log(store)
+
+type accountDetails = [string , string , boolean , string?]
+
+const staf : accountDetails[] = [
+    ["Mashruf" , "The was a medical bilar" , true] ,
+    ["Aslam" , "The was a developer" , true],
+    ["Khan" , "The was a engineer" , true]
+]
+console.log(staf)
+
+type paymount = [accountDetails  , ...number[]]
+
