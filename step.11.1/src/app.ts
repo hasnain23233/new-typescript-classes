379de9330794c706debbcf728c561f9b9e4da638
Hasnain Khushi <hasnainkhushii@gmail.com>
2026-02-11 21:26:29 +0500

    working on the strutul typing in typescript

---
+++ step.11.1/src/app.ts
@@ -0,0 +1,34 @@
+console.log("In this class we learn about the structul typing")
+
+class Human {
+    name: string
+
+    constructor(name: string){
+        this.name = name
+    }
+
+    naming (){
+        console.log(this.name + " Is best human in the world and he is a best web developer")
+    }
+}
+
+class Animal {
+    name: string
+    age: number
+
+    constructor(name: string , age: number){
+        this.name = name
+        this.age = age
+    }
+    eat (){
+        console.log(this.name + " is a animal is eating and the age of animal is " + this.age )
+    }
+}
+
+let human = new Human("Hasnain")
+human.naming()
+
+let animal = new Animal("dog" , 38)
+animal.eat()
+console.log(animal.age)
+console.log(animal.name)
